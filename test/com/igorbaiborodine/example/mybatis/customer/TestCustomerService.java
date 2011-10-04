7b765d8f83bbf12dd3e373f2c98eb60e45e36ae4
Igor Baiborodine <igor.baiborodine@gmail.com>
2011-10-03 21:38:56 -0400

    Added testFindCustomer and testAddCustomer methods.

--- test/com/igorbaiborodine/example/mybatis/customer/TestCustomerService.java
+++ test/com/igorbaiborodine/example/mybatis/customer/TestCustomerService.java
@@ -19,106 +19,93 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionManager;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import javax.annotation.Resource;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.igorbaiborodine.example.mybatis.address.Address;
 import com.igorbaiborodine.example.mybatis.exceptions.ServiceException;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/spring/application-context.xml") 
 public class TestCustomerService {
-	private static CustomerServiceImpl customerDao;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		SqlSessionManager sessionManager = null;
-		
-		try {
-			Reader reader = Resources.getResourceAsReader("mybatisConfig.xml");
-			sessionManager = SqlSessionManager.newInstance(reader);
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		//customerDao = new CustomerDaoImpl(sessionManager);
-	}
-	
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		customerDao = null;
-	}
+	@Resource
+	private CustomerServiceImpl _customerService;
 
 	@Test
-	public void testGetCustomer() throws ServiceException {
+	public void testFindCustomer() throws ServiceException {
 		
 		// 1, 1, MARY, SMITH, MARY.SMITH@sakilacustomer.org, 5, 0, 2006-02-14 22:04:36, 2011-09-21 18:52:54
 		short customerId = 1;
-		Customer customer = customerDao.getCustomer(customerId);
-		assertNotNull("test get customer failed - customer must not be null", customer);
-		assertEquals("test get customer failed - first name must not be different from MARY", 
+		Customer customer = _customerService.findCustomer(customerId);
+		assertNotNull("test find customer failed - customer must not be null", customer);
+		assertEquals("test find customer failed - first name must not be different from MARY", 
 				"MARY", customer.getFirstName());
+		assertEquals("test find customer failed - first name must not be different from SMITH", 
+				"SMITH", customer.getLastName());
 		
 		customerId = -1;
-		customer = customerDao.getCustomer(customerId);
-		assertNull("test get customer failed - customer must be null", customer);
+		customer = _customerService.findCustomer(customerId);
+		assertNull("test find customer failed - customer must be null", customer);
 	}
 	
 	@Test
 	public void testAddCustomer() throws ServiceException {
+
 		int random = (new Random()).nextInt(1000);
 		Customer customer = createCustomer(random);
 		Address address = createAddress(random);
 		
-		short newCustomerId = customerDao.addCustomer(customer, address);
+		short newCustomerId = _customerService.addCustomer(customer, address);
 		assertTrue("test add customer failed - new customer id must be greater than 0",
 				newCustomerId > 0);
 	}
 	
 	private Address createAddress(int random_) {
-		Address address = new Address();
 		
-		address.setAddress(random_ + " Test Ave.");
-		address.setCityId((short) 1);
-		address.setDistrict("Vermont");
-		address.setPostalCode("30101");
-		address.setPhone("8887777" + random_);
+		Address address = new Address();
+		address.setAddress(random_ + " Test Service Ave.");
+		address.setCityId(new Short("1"));
+		address.setDistrict("MyBatis-Spring");
+		address.setPostalCode("33333");
+		address.setPhone("5556666666");
 		
 		return address;
 	}
 
 	private Customer createCustomer(int random_) {
-		Customer customer = new Customer();
 		
-		String firstName = "FIRST_NAME_" + random_;
-		String lastName = "LAST_NAME_" + random_;
+		Customer customer = new Customer();
+		String firstName = "FIRST_NAME_ANT_" + random_;
+		String lastName = "LAST_NAME_ANT_" + random_;
+
 		customer.setFirstName(firstName);
 		customer.setLastName(lastName);
-		
-		customer.setEmail(firstName + "." + lastName + "@sakilacustomer.org");
-		customer.setStoreId((byte) 2);
+		customer.setEmail(random_ + "_test.service@sakilacustomer.org");
+		customer.setStoreId(new Byte("1"));
 		customer.setActive(true);
 		customer.setCreateDate(new Date());
-		
+
 		return customer;
 	}
 	
 	@Test 
 	public void testGetCustomerRewardsReport() throws ServiceException {
-		byte minMonthlyPurchases = 7;
-		double minDollarAmountPurchased = 20.0;
-		
-		List<Customer> rewardsReport = customerDao.getCustomerRewardsReport(
-				minMonthlyPurchases, minDollarAmountPurchased);
-		assertEquals("test get customer rewards report failed - record count must be equal to 2", 
-				2, rewardsReport.size());
+//		byte minMonthlyPurchases = 7;
+//		double minDollarAmountPurchased = 20.0;
+//		
+//		List<Customer> rewardsReport = customerDao.getCustomerRewardsReport(
+//				minMonthlyPurchases, minDollarAmountPurchased);
+//		assertEquals("test get customer rewards report failed - record count must be equal to 2", 
+//				2, rewardsReport.size());
+		fail("not implemented");
 	}
 }
